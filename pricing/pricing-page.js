27170d0279314d557e2e77f3163b9cc80c6113ea
Ramu Narasinga <ramu@zuuba.com>
2019-06-21 17:29:58 +0530

    added svg icons

--- pricing/pricing-page.js
+++ pricing/pricing-page.js
@@ -172,8 +172,12 @@ class CategoryFeatures extends React.Component {
                             <div sm="2" className="category-feature-xs">
                                 <div>{item.values[val]}&nbsp;{item.name}&nbsp;
                                     <Tooltip message={item.info} position={'top'}>
-                                        <span className="custom-info-icon-xs" aria-hidden="true" data-tip={item.info}>
-                                            <p style={{position: 'relative', bottom: '15px'}}>i</p>
+                                        <span>
+                                            <svg width="20px" height="20px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+     
+                                                <path d="M12.13 11.59c-.16 1.25-1.78 2.53-3.03 2.57-2.93.04.79-4.7-.36-5.79.56-.21 1.88-.54 1.88.44 0 .82-.5 1.74-.74 2.51-1.22 3.84 2.25-.17 2.26-.14.02.03.02.17-.01.41-.05.36.03-.24 0 0zm-.57-5.92c0 1-2.2 1.48-2.2.36 0-1.03 2.2-1.49 2.2-.36z" />
+                                                    <circle cx="10" cy="10" r="9" fill="none" stroke="currentColor" stroke-width="1.1" />
+                                            </svg>
                                         </span>
                                     </Tooltip>
                                 </div>
@@ -406,8 +410,12 @@ class PricingPage extends React.Component {
 
                                     <div  className="category-name">{item.name}&nbsp; 
                                     <Tooltip message={item.info} position={'top'}>
-                                        <span className="custom-info-icon" aria-hidden="true" data-tip={item.info}>
-                                            <p style={{position: 'relative', bottom: '7px'}}>i</p>
+                                        <span>
+                                            <svg width="20px" height="20px" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+     
+                                                <path d="M12.13 11.59c-.16 1.25-1.78 2.53-3.03 2.57-2.93.04.79-4.7-.36-5.79.56-.21 1.88-.54 1.88.44 0 .82-.5 1.74-.74 2.51-1.22 3.84 2.25-.17 2.26-.14.02.03.02.17-.01.41-.05.36.03-.24 0 0zm-.57-5.92c0 1-2.2 1.48-2.2.36 0-1.03 2.2-1.49 2.2-.36z" />
+                                                    <circle cx="10" cy="10" r="9" fill="none" stroke="currentColor" stroke-width="1.1" />
+                                            </svg>
                                         </span>
                                     </Tooltip>
                                     </div>
