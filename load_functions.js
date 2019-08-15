edc4a490260c15cd078479749ab6491e82366e47
Philippe Trounev <phil@docsie.io>
2019-08-15 13:15:59 -0400

    Removed addroll

--- load_functions.js
+++ load_functions.js
@@ -74,24 +74,6 @@ function setSubmit(id1, id2) {
 }
 
 
-adroll_adv_id = "PPYP5KFCD5F2XEEP3365PB";
-adroll_pix_id = "BWXONQKGSRDHBCVOPJUI7W";
-
-(function () {
-    var _onload = function () {
-        if (document.readyState && !/loaded|complete/.test(document.readyState)) { setTimeout(_onload, 10); return }
-        if (!window.__adroll_loaded) { __adroll_loaded = true; setTimeout(_onload, 50); return }
-        var scr = document.createElement("script");
-        var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-        scr.setAttribute('async', 'true');
-        scr.type = "text/javascript";
-        scr.src = host + "/j/roundtrip.js";
-        ((document.getElementsByTagName('head') || [null])[0] ||
-            document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-    };
-    if (window.addEventListener) { window.addEventListener('load', _onload, false); }
-    else { window.attachEvent('onload', _onload) }
-}());
 
 // check and set cookies for GDPR banner
 function setCookie(cname, cvalue) {
