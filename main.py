290e16916b5120e1f8d701047cc6a20393855c38
Nina Loukachina <nina@docsie.io>
2021-02-11 12:17:35 -0500

    updates to content.

--- main.py
+++ main.py
@@ -11,18 +11,18 @@ cwd = os.getcwd()
 
 
 if __name__ == "__main__":
-    d = 'locale'
-    dirs = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
-    for loc in dirs:
-        locale = loc.replace('locale/', '')
-        if locale == 'ja_JP':
-            loc_name = '/jp'
-        else:
-            loc_name = '/' + locale
-        site = Site.make_site(searchpath='./src', env_globals={"locale": loc_name},locale=locale, extensions=['jinja2.ext.i18n'])
-
-        # enable automatic reloading
-        site.render(use_reloader=False)
+    # d = 'locale'
+    # dirs = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
+    # for loc in dirs:
+    #     locale = loc.replace('locale/', '')
+    #     if locale == 'ja_JP':
+    #         loc_name = '/jp'
+    #     else:
+    #         loc_name = '/' + locale
+    #     site = Site.make_site(searchpath='./src', env_globals={"locale": loc_name},locale=locale, extensions=['jinja2.ext.i18n'])
+    #
+    #     # enable automatic reloading
+    #     site.render(use_reloader=False)
 
     site = Site.make_site(searchpath='./src',
                           extensions=['jinja2.ext.i18n', 'jinja2.ext.autoescape', 'jinja2.ext.with_'])
