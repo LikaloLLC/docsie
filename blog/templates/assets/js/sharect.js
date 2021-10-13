3a213df7ac9fb25c30445390204be40ea1f50924
Nina Loukachina <nina@docsie.io>
2021-10-13 15:21:57 -0400

    some change

--- blog/templates/assets/js/sharect.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-    let sharectConfig = window['sharectConfig']
-
-    Sharect.init()
-
-    Sharect.config({
-        ...sharectConfig
-    }).init()
-})
